04bc3ca7aa5b203f6f750f027bed0d9d160e8592
Анна Котляр <kotlyaranya1771@gmail.com>
2023-07-20 19:19:44 +0300

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+const fileInclude = require('gulp-file-include');
+
+gulp.task('html', function () {
+  return gulp.src('./src/index.html')
+    .pipe(fileInclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('./dist'));
+});
