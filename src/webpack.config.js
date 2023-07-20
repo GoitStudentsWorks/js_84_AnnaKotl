598c9069c06e8299f352d0f477aeddc8aa75abe2
Анна Котляр <kotlyaranya1771@gmail.com>
2023-07-20 18:59:52 +0300

    webpack

---
+++ src/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new BundleAnalyzerPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    port: 9000
+  }
+};
