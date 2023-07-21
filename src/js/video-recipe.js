1662339c83e35cb20a30f975c32dcdbb0134cf26
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-07-21 16:40:16 +0300

    п

--- src/js/video-recipe.js
+++ src/js/video-recipe.js
@@ -1,19 +1,19 @@
-(() =>{
+(() => {
     const refs = {
-        openModalbtn: document.querySelector('[data-modal-open-video-recipes]'),
-        closeModalbtn: document.querySelector('[data-modal-close-video-recipes]'),
-        modal: document.querySelector('[modal-video-recipes]')
-    }
-
+      openModalBtn: document.querySelector('[data-modal-open-reciepts]'),
+      closeModalBtn: document.querySelector('[data-modal-close-reciepts]'),
+      modal: document.querySelector('[data-modal-reciepts]'),
+    };
+  
     refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.openSecondModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
-
-    function toggleModal(){
-        refs.modal.classList.toggle('is-hidden')
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
     }
-})
-const modalVideoRecipes = document.querySelector('.modal-video-recipes')
+  })();
+
+const modalVideoRecipes = document.querySelector('.video-recipe')
 
 function showRecieptInfo(info){
     const modalVideoRecipesMarkup = info.map(({
@@ -28,6 +28,42 @@ function showRecieptInfo(info){
         return `<h2 class="title">${title}</h2>
         <iframe width="467" height="250"
 src=${youtube}>
-</iframe>`
-    })
+</iframe>
+<p class="tags">#${tags}</p>
+<p>${rating}</p>
+<svg class="reciepts-stars1" width="18" height="18">
+        <use href="/src/images/icons-modal-reciepts.svg#icon-Star--converted"></use>
+    </svg>
+    <svg class="reciepts-stars2" width="18" height="18">
+        <use href="/src/images/icons-modal-reciepts.svg#icon-Star--converted"></use>
+    </svg>
+    <svg class="reciepts-stars3" width="18" height="18">
+        <use href="/src/images/icons-modal-reciepts.svg#icon-Star--converted"></use>
+    </svg>
+    <svg class="reciepts-stars4" width="18" height="18">
+        <use href="/src/images/icons-modal-reciepts.svg#icon-Star--converted"></use>
+    </svg>
+    <svg class="reciepts-stars5" width="18" height="18">
+        <use href="/src/images/icons-modal-reciepts.svg#icon-Star--converted"></use>
+</svg>
+<p class="reciepts-time">${time} min</p>
+<ul>
+<li><p class="reciepts-name">${ingredients.name}</p><pclass="reciepts-measure">${ingredients.measure}</pclass=></li>
+</ul>
+<p class="reciepts-instructions">${instructions}</p>
+<button class="add-to-favourite">Add to favorite</button>
+<button class="give-a-rating">Give a rating</button>
+`}).join('')
+modalVideoRecipes.insertAdjacentHTML('beforeend', modalVideoRecipesMarkup)
+}
+
+function recieptsOfFood (){
+    const BASE_URL_RECIEPTS = 'https://tasty-treats-backend.p.goit.global/api/recipes/6462a8f74c3d0ddd28897fd9'
+
+    return fetch(BASE_URL_RECIEPTS)
+    .then(resp => {
+        if (!resp.ok) throw new Error(resp.status);
+        return resp.json();
+      })
+      .catch(error  => console.log(error))
 }
