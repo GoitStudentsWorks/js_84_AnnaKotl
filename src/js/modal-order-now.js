dd5caec31c4af1c93a9c0584f3bf95b50d2dd076
OlegMalyshok <olegmalyshok@gmail.com>
2023-07-21 22:29:53 +0300

    hero-modal-order-now

--- src/js/modal-order-now.js
+++ src/js/modal-order-now.js
@@ -1,1 +1,20 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-ordernow]'),
+    openSecondModalBtn: document.querySelector(
+      '[data-modal-open-ordernowsecond]'
+    ),
+
+    closeModalBtn: document.querySelector('[data-modal-close-ordernow]'),
+    modal: document.querySelector('[data-modal-ordernow]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openSecondModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
 console.log('modal-order-now.js work');
