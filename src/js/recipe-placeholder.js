6ddd02a093261f50d5f0317f1c9fdb95e285d39b
unmin2 <125081601+unmin2@users.noreply.github.com>
2023-07-21 23:03:07 +0300

    add code

--- src/js/recipe-placeholder.js
+++ src/js/recipe-placeholder.js
@@ -1,1 +1,21 @@
-console.log('recipe-placeholder.js work');
+async function getData() {
+    const response = await fetch('');
+    const data = await response.json();
+    return data;
+}
+async function main() {
+    const postdData = await getData();
+    let currentPage = 1;
+    let rows = 9;
+    let colns = 3;
+    function displayList(arrData, rowPerPega, page) {
+        const postsEl = document.querySelector('.posts');
+        const start = rowPerPega * page;
+        const end = start + rowPerPega;
+        const paginatedData = arrData.slice(start, end);
+        paginatedData.array.forEach( (el) => {
+            const postEl = document.createElement('.div')
+        });
+    }
+
+}
