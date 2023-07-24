41a4a530ba0eb85697fe7d848e958ffcc17292e8
Tetiana-Nazarenko <nnn27725@gmail.com>
2023-07-24 23:07:46 +0300

    add card.addEventListener

--- src/js/popular-recipes.js
+++ src/js/popular-recipes.js
@@ -3,48 +3,52 @@
 // ЗРОБИТИ ПУШ ПІСЛЯ ВИПРАВЛЕНЬ І БЕЗ ПОМИЛОК
 //-----------------------------------------
 
-// // import { schowModalAboutReciepts } from './video-recipe';
+import { showModalAboutReciepts } from './video-recipe';
 
-// const url = 'https://tasty-treats-backend.p.goit.global/api/recipes/popular';
+const url = 'https://tasty-treats-backend.p.goit.global/api/recipes/popular';
 
-// const pop_recipe_info = document.querySelector('.popular-recipes');
+const pop_recipe_info = document.querySelector('.popular-recipes');
 
-// function fetchPopularRecipes() {
-//   return fetch(`${url}`).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   });
-// }
+function fetchPopularRecipes() {
+  return fetch(`${url}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
 
-// fetchPopularRecipes()
-//   .then(data => {
-//     console.log(data);
-//     //pop_recipe_info.insertAdjacentHTML('beforeend', renderPopularRecipes(data));
-//     pop_recipe_info.innerHTML = renderPopularRecipes(data);
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   });
+fetchPopularRecipes()
+  .then(data => {
+    console.log(data);
+    //pop_recipe_info.insertAdjacentHTML('beforeend', renderPopularRecipes(data));
+    pop_recipe_info.innerHTML = renderPopularRecipes(data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
-// function renderPopularRecipes(recipes) {
-//   return recipes
-//     .map(({ preview, title, description }) => {
-//       return `<li class="pop-recipe-link">
-//       <div class="pop-recipe-card"> 
-//     <img class="img-pop-recipe" src="${preview}" width="64" height="64" alt="">
-//     <div class="text-pop-recipe">  
-//     <h3 class="title-pop-recipe">${title}</h3>
-//     <p class="description-pop-recipe">${description}</p>
-//     </div>
-//     </div>
-//   </li>`;
-//     })
-//     .join('');
-// }
-
+function renderPopularRecipes(recipes) {
+  return recipes
+    .map(({ id, preview, title, description }) => {
+      return `<li class="pop-recipe-link" id="${id}">
+      <div class="pop-recipe-card"> 
+    <img class="img-pop-recipe" src="${preview}" width="64" height="64" alt="">
+    <div class="text-pop-recipe">  
+    <h3 class="title-pop-recipe">${title}</h3>
+    <p class="description-pop-recipe">${description}</p>
+    </div>
+    </div>
+  </li>`;
+    })
+    .join('');
+}
 
 // // тут може бути помилка:
-// const openResipe = document.querySelector('.pop-recipe-link');
-// openResipe.addEventListener('click', schowModalAboutReciepts);
+const openResipesCards = document.getElementById(`${id}`);
+//showModalAboutReciepts('6462a8f74c3d0ddd28898040');
+// const openResipesCards = document.querySelectorAll('li');
+
+openResipesCards.forEach(card => {
+  card.addEventListener('click', showModalAboutReciepts(card.id));
+});
--- src/js/video-recipe.js
+++ src/js/video-recipe.js
@@ -1,18 +1,17 @@
-export
-let refs = {
-addToFavoriteBtn: document.querySelector('.btn-add'),
-removeFromFavoriteBtn:document.querySelector('.btn-outline-remove'),
-recieptsTitle: document.querySelector('.reciepts-title'),
-backdropRecipe: document.querySelector('.backdrop-video-recipes'),
-modalRecipe:document.querySelector('.modal-video-recipe'),
-closeBtn: document.querySelector('.modal-video-recipes-close-button'),
-tagsRecipe: document.querySelector('.tags-recipe'),
-ratingRecipe: document.querySelector('.rating-recipe'),
-minutesRecipe: document.querySelector('.minutes-recipe'),
-ingredientsRecipe: document.querySelector('.ingredients-recipe'),
-instructionsRecipe: document.querySelector('.instructions-recipe'),
-videoRecipe: document.querySelector('.video-recipe'),
-}
+export let refs = {
+  addToFavoriteBtn: document.querySelector('.btn-add'),
+  removeFromFavoriteBtn: document.querySelector('.btn-outline-remove'),
+  recieptsTitle: document.querySelector('.reciepts-title'),
+  backdropRecipe: document.querySelector('.backdrop-video-recipes'),
+  modalRecipe: document.querySelector('.modal-video-recipe'),
+  closeBtn: document.querySelector('.modal-video-recipes-close-button'),
+  tagsRecipe: document.querySelector('.tags-recipe'),
+  ratingRecipe: document.querySelector('.rating-recipe'),
+  minutesRecipe: document.querySelector('.minutes-recipe'),
+  ingredientsRecipe: document.querySelector('.ingredients-recipe'),
+  instructionsRecipe: document.querySelector('.instructions-recipe'),
+  videoRecipe: document.querySelector('.video-recipe'),
+};
 
 refs.closeBtn.addEventListener('click', closeModalClose);
 refs.backdropRecipe.addEventListener('click', clickBackdropClick);
@@ -39,64 +38,63 @@ function onEscPress(key) {
   }
 }
 function clickBackdropClick(element) {
-  if(element.currentTarget === element.target)
-  closeModalClose()
+  if (element.currentTarget === element.target) closeModalClose();
 }
 
 export function showModalAboutReciepts(id) {
-    recieptsOfFood(id).then(data => {
+  recieptsOfFood(id).then(data => {
     //   isFavorite(data._id);
-      renderRanting(data);
-      markUpRating();
-      renderIngridient(data);
-      renderHashtags(data);
-      renderText(data);
-      openModalOpen();
-      renderVIDEO(data);
-      recipeId = data._id;
-    });
+    renderRanting(data);
+    markUpRating();
+    renderIngridient(data);
+    renderHashtags(data);
+    renderText(data);
+    openModalOpen();
+    renderVIDEO(data);
+    recipeId = data._id;
+  });
 }
 
-async function recieptsOfFood(id){
-    const resp = await fetch(
-        `https://tasty-treats-backend.p.goit.global/api/recipes/${id}`
-      );
-      const data = await resp.json();
-      return data;
+async function recieptsOfFood(id) {
+  const resp = await fetch(
+    `https://tasty-treats-backend.p.goit.global/api/recipes/${id}`
+  );
+  const data = await resp.json();
+  return data;
 }
 
 function renderText(data) {
-    refs.recieptsTitle.textContent = data.title; 
-    refs.videoRecipe.src = data.preview; 
-    refs.instructionsRecipe.textContent = data.instructions; 
-    refs.minutesRecipe.textContent = data.time + ' min'; 
-  }
+  refs.recieptsTitle.textContent = data.title;
+  refs.videoRecipe.src = data.preview;
+  refs.instructionsRecipe.textContent = data.instructions;
+  refs.minutesRecipe.textContent = data.time + ' min';
+}
 
-  let ratings;
-  let ratingActive, ratingValue;
-  
-  function initRating(rating) {
-    initValues(rating);
-    if (ratingValue.innerHTML > 5) {
-      ratingValue.innerHTML = 5;
-    }
-    const ratingActiveWidth = ratingValue.innerHTML / 0.05;
-  
-    ratingActive.style.width = `${ratingActiveWidth}%`;
+let ratings;
+let ratingActive, ratingValue;
+
+function initRating(rating) {
+  initValues(rating);
+  if (ratingValue.innerHTML > 5) {
+    ratingValue.innerHTML = 5;
   }
-  
-  function initValues(rating) {
-    ratingActive = rating.querySelector('.rating__active');
-    ratingValue = rating.querySelector('.rating__value');
-  }
-  
-  export function markUpRating() {
-    ratings = document.querySelectorAll('.rating');
-    ratings.forEach(item => {
-      const rating = item;
-      initRating(rating);
-    });
-  }
+  const ratingActiveWidth = ratingValue.innerHTML / 0.05;
+
+  ratingActive.style.width = `${ratingActiveWidth}%`;
+}
+
+function initValues(rating) {
+  ratingActive = rating.querySelector('.rating__active');
+  ratingValue = rating.querySelector('.rating__value');
+}
+
+export function markUpRating() {
+  ratings = document.querySelectorAll('.rating');
+  ratings.forEach(item => {
+    const rating = item;
+    initRating(rating);
+  });
+}
 
 function getKeyYouTybe(url) {
   let indexLast = url.split('').length;
@@ -118,8 +116,8 @@ allowfullscreen
   refs.videoRecipe.innerHTML = markUp;
 }
 
-  function renderRanting(data) {
-    let markupR = `
+function renderRanting(data) {
+  let markupR = `
     <div class="cards__rating rating">
     <div class="rating__value detail">${data.rating}</div>
     <div class="rating__body">
@@ -158,27 +156,31 @@ allowfullscreen
       </div>
     </div>
   </div>`;
-    refs.ratingRecipe.innerHTML = markupR; // Change refs.ratingBox to refs.ratingRecipe
-  }
-  
-  function renderIngridient(data) {
-    const markup = data.ingredients
-      .map(({ measure, name }) => `
+  refs.ratingRecipe.innerHTML = markupR; // Change refs.ratingBox to refs.ratingRecipe
+}
+
+function renderIngridient(data) {
+  const markup = data.ingredients
+    .map(
+      ({ measure, name }) => `
         <li class="recipes-subtitle">
           ${name}
           <p class="recipes-inf">${measure}</p>
         </li>
-      `)
-      .join('');
-    refs.ingredientsRecipe.innerHTML = markup; // Change refs.IngredientBox to refs.ingredientsRecipe
-  }
-  
-  function renderHashtags(data) {
-    if (data.tags.length === 0) {
-      return;
-    }
-    const markup = data.tags.map(tag => ` <li class="hashtags">#${tag}</li>`).join('');
-    refs.tagsRecipe.innerHTML = markup; // Change refs.hashtagsBox to refs.tagsRecipe
-  }
+      `
+    )
+    .join('');
+  refs.ingredientsRecipe.innerHTML = markup; // Change refs.IngredientBox to refs.ingredientsRecipe
+}
 
-showModalAboutReciepts('6462a8f74c3d0ddd28897fc8')
+function renderHashtags(data) {
+  if (data.tags.length === 0) {
+    return;
+  }
+  const markup = data.tags
+    .map(tag => ` <li class="hashtags">#${tag}</li>`)
+    .join('');
+  refs.tagsRecipe.innerHTML = markup; // Change refs.hashtagsBox to refs.tagsRecipe
+}
+
+showModalAboutReciepts('6462a8f74c3d0ddd28897fc8');
