986de43357c923f934ac9b9f480a95867ad54f52
OlegMalyshok <olegmalyshok@gmail.com>
2023-07-24 19:02:56 +0300

    hero-modal-order-now
    Доробив js

--- src/js/modal-order-now.js
+++ src/js/modal-order-now.js
@@ -1,3 +1,5 @@
+// Js для відкриття/закриття модального вікна
+
 const openModalBtn = document.getElementById('openModalOrderNowBtn');
 const closeModalBtn = document.getElementById('closeModalOrderNowBtn');
 const modal = document.getElementById('modalordernow');
@@ -12,5 +14,52 @@ function closeModalOrderNow() {
 
 openModalBtn.addEventListener('click', openModalOrderNow);
 closeModalBtn.addEventListener('click', closeModalOrderNow);
+
+//Js для відправки введених даних на бекенд
+
+const feedbackForm = document.querySelector('.ordernow-form');
+const nameInput = feedbackForm.querySelector('input[name="name"]');
+const phoneInput = feedbackForm.querySelector('input[name="phone"]');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageInput = feedbackForm.querySelector('textarea[name="message"]');
+
+feedbackForm.addEventListener('input', () => {
+  const formData = {
+    name: nameInput.value,
+    phone: phoneInput.value,
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+const savedFormData = localStorage.getItem('feedback-form-state');
+if (savedFormData) {
+  const formData = JSON.parse(savedFormData);
+  nameInput.value = formData.name;
+  phoneInput.value = formData.phone;
+  emailInput.value = formData.email;
+  messageInput.value = formData.message;
+}
+
+feedbackForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formData = {
+    name: nameInput.value,
+    phone: phoneInput.value,
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  localStorage.removeItem('feedback-form-state');
+
+  nameInput.value = '';
+  phoneInput.value = '';
+  emailInput.value = '';
+  messageInput.value = '';
+
+  console.log(formData);
+});
 
 console.log(openModalBtn);
