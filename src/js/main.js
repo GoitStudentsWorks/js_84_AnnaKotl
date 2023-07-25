0ad26f29980918bdd11bc79db2a4f7943f2fa732
Анна Котляр <kotlyaranya1771@gmail.com>
2023-07-25 12:36:26 +0300

    rating js done and need fix

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@ import './filter-of-favourite-foods.js';
 import './filter.js';
 import './time.js';
 import './header.js';
-
 import './hero-switch.js';
 import './hero.js';
 import './modal-order-now.js';
@@ -10,19 +9,10 @@ import './popular-recipes.js';
 import './recipe-placeholder.js';
 import './scroll-categories.js';
 import './video-recipe.js';
-
 import './rating.js';
-
 import './all-foods.js';
 import './heder-modal-menu.js';
 import './scroll-to-start.js';
-
-// Fevorites
-// import './js-file-fevorites/one-fevorites.js';
-// import './js-file-fevorites/two-fevorites.js';
-// import './js-file-fevorites/three-fevorites.js';
-// import './js-file-fevorites/four-fevorites.js';
-// import './js-file-fevorites/five-fevorites.js';
 
 // All API js files
 import './api-kay-js-files/api-filter-of-favourite-foods.js'
