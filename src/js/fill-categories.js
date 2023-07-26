fa8ff298af5129783e9a05039df11756ca7533f2
Kateryna <katerynababiak@gmail.com>
2023-07-27 02:18:00 +0300

    update fill-categories js

--- src/js/fill-categories.js
+++ src/js/fill-categories.js
@@ -4,27 +4,47 @@ import { Notify } from 'notiflix';
 
 
 const refs = {
-  categroyList: document.querySelector('.category-list'),
+  categoryList: document.querySelector('.category-list'),
 };
 
 
-async function fillCategoriesList() {
-  try {
-    const categories = await fetchCategories();
-    renderCategoriesList(categories);
-  } catch (err) {
-    console.log(err);
-    Notify.failure('Something went wrong. Please try again');
-  }
-};
+// async function fillCategoriesList() {
+//   try {
+//     const categories = await fetchCategories();
+//     renderCategoriesList(categories);
+//   } catch (err) {
+//     console.log(err);
+//     Notify.failure('Something went wrong. Please try again');
+//   }
+// };
 
 
-fillCategoriesList();
+// fillCategoriesList();
 
 
-function renderCategoriesList(categories) {
-  refs.categroyList.insertAdjacentHTML(
-    'beforeend',
-    TemplateCategories(categories)
-  );
-};
+// function renderCategoriesList(categories) {
+//   refs.categoryList.insertAdjacentHTML(
+//     'beforeend',
+//     TemplateCategories(categories)
+//   );
+// };
+
+
+async function renderCategoriesList() {
+  try { 
+    const arrMk = await  fetchCategories();
+    // console.log(arrMk);
+    const markup = arrMk
+    .map(evt => {
+      return `<li class='cat-items'>
+      <button class='category-btn' value = ${evt._id}>${evt.name}</button>
+    </li>`}).join('');
+  refs.categoryList.innerHTML = markup;
+} catch (error) {
+  console.error("Error:", error);
+  Notify.failure('Something went wrong. Please try again');
+}
+
+}
+
+renderCategoriesList();
