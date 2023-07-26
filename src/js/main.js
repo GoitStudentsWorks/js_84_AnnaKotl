d979e76e73045f2e0fedf0e23e81291fef0ab110
Анна Котляр <kotlyaranya1771@gmail.com>
2023-07-26 14:14:04 +0300

    Revert "addToFav"
    This reverts commit 1c0378d97c0eafe6ad54984fbb08b89c4528035e.

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,6 @@ import './rating.js';
 import './all-foods.js';
 import './heder-modal-menu.js';
 import './scroll-to-start.js';
-import './addToFavourite.js';
 // All API js files
 import './api-kay-js-files/api-filter-of-favourite-foods.js'
 import './api-kay-js-files/api-filter.js';
